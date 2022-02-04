83c5bad87d9b2a443acbf2af41e637209057b09f
maco1503 <andreimacovei1503@gmail.com>
2022-02-04 18:38:13 +0200

    commit

--- index.js
+++ index.js
@@ -11,14 +11,26 @@ const port = 8080;
 
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
+  
+var upload = multer({ storage: storage });
+
+
 
 app.use('/blogPostsRouter', postRouter);
 app.use('/', function(req, res){
     res.render('makePostPage');
-});
- 
+});    
 
 app.get(('/'),(req,res)=>{
     res.send();
@@ -27,5 +39,8 @@ app.get(('/'),(req,res)=>{
 
 app.listen(port, () =>{
     console.log("merge");
-    mongoose.connect(process.env.MONGODB_URL);
+    mongoose.connect(process.env.MONGODB_URL,
+    { useNewUrlParser: true, useUnifiedTopology: true }, err => {
+        console.log('connected')
+    });
 });
